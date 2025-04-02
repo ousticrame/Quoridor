38be67b1a55730afafdb36b8af9099d4b0a1521d
Aniss <aniss1.outaleb@epita.fr>
2025-04-02 11:44:14 +0200

    feat with rest

--- ppc/src/main/java/org/example/Main.java
+++ ppc/src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         RoundRobinScheduler.run();
+        System.out.println("RunWithRest");
+        RoundRobinScheduler.runWithRest();
     }
 }
