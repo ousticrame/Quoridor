31c503b2b9fceb7fbd928f8c8b5ce297f6459a6c
Aniss <aniss1.outaleb@epita.fr>
2025-04-10 21:46:48 +0200

    add details

--- choco/src/main/java/org/example/Main.java
+++ choco/src/main/java/org/example/Main.java
@@ -5,11 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-//        List<Schedule.Team> teams = new ArrayList<>();
-//        for (int i = 1; i <= 6; i++) {
-//            teams.add(new Schedule.Team("team" + i, new Schedule.Stadium("stadium" + i, new ArrayList<>())));
-//        }
-        // Schedule.main();// breaks élevés pour test
+        // RoundRobinScheduler.optiRun();
+        RoundRobinScheduler.run();
     }
 }
